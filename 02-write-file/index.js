3795107e914a01cb788ad5465f7b9ec982b532ff
spiderVS <fuzzy003@gmail.com>
2021-11-04 20:31:56 +0300

    fix: correct exit after CTRL+C

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,23 +6,21 @@ const { stdin: input, stdout: output } = require('process');
 const FILE = 'text.txt';
 const fileName = path.join(__dirname, FILE);
 
-output.write('Please, enter your text below:\n');
+output.write('Please, enter your text below:\n\n');
+
 try {
   const stream = fs.createWriteStream(fileName, 'utf8');
   const rl = readline.createInterface({ input, stream });
 
   rl.on('line', (line) => {
-    if (line === 'exit') {
-      rl.close();
-      process.exit();
-    }
-    else stream.write(line+'\n')
+    if (line === 'exit') exitHandler();
+    else stream.write(`${line}\n`);
   });
-  // rl.on('close', () => {
-  //   process.exit();
-  // })
-
-  process.on('exit', () => output.write('Thank you and Good bye!!!'));
+  const exitHandler = () => {
+    output.write('\n\nThank you and Good bye!!!\n');
+    rl.close();
+  };
+  process.on('SIGINT', exitHandler);
 
 } catch (err) {
   throw err;
