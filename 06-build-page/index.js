3984933e34b6a3e392558c02098564c387f51d84
spiderVS <fuzzy003@gmail.com>
2021-11-05 21:40:42 +0300

    feat: start 06-build-page - variable declaration

---
+++ 06-build-page/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+
+const DEST_FOLDER_NAME = 'project-dist';
+const BUNDLE_HTML_NAME = 'index.html';
+const BUNDLE_CSS_NAME = 'style.css';
+
+const ENRTY_HTML_NAME = 'template.html';
+const STYLES_FOLDER_NAME = 'styles';
+const ASSETS_FOLDER_NAME = 'assets';
+const COMPONENTS_FOLDER_NAME = 'assets';
